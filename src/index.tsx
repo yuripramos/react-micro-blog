2d0524a677f31d917ebc0b1e32c9b8ea3bc0c099
Yuri Ramos <yuri.pramos@gmail.com>
2020-04-10 17:04:44 -0300

    configuring ts extension inside webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 // import './styles.scss';
 
 import Store from './store';
-import Header from '../components/Header';
-import Main from '../components/Main';
-import Footer from '../components/Footer';
-
-export default () => (
+// import Header from '../components/Header';
+// import Main from '../components/Main';
+// import Footer from '../components/Footer';
+const App = () => (
   <div id="app" >
     <Store>
-      <Header />
-      < Main />
-      <Footer />
+      {/* <Header />
+      <Main />
+      <Footer /> */}
+      oioioioioioioioioioioioioioi
     </Store>
   </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
