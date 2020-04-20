46e8681bf8d30610e3570ffc68dd90fa62c0f19a
Yuri Ramos <yuri.pramos@gmail.com>
2020-04-19 21:28:34 -0300

    updating jest preset and finishing first test

--- src/store/index.test.tsx
+++ src/store/index.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 
 import Store from "./index";
 
+afterEach(cleanup);
 describe("Store", () => {
 
-  const props = {
-    div: <div> test </div>
-  }
+  const children = () => <div>{'children'}</div>;
+
   it("render store", () => {
     expect(() => {
-      render(<Store children={props} />);
+      render(<Store children={children} />);
     }).not.toThrow();
   });
 });
